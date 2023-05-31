64aa4b129ef26eec552f5f9519a2c633b4ef244d
drake0401 <vuhoanglong72@gmail.com>
2023-05-31 20:20:05 +0700

    feat: Singleton and Template

--- src/main/java/subsystem/interbank/InterbankSubsystemController.java
+++ src/main/java/subsystem/interbank/InterbankSubsystemController.java
@@ -12,6 +12,8 @@ public class InterbankSubsystemController {
 		return null;
 	}
 
+	// co the ap dung mau Template do khi có nhieu hon phuong thuc thanh toan, payorder giua ca pt có 
+	// the khac nhau nhung deu qua 3 buoc nhu tren
 	public PaymentTransaction payOrder(CreditCard card, int amount, String contents) {
 		String requestPayload = interbankPayloadConverter.convertToRequestPayload(card, amount, contents);
 		String responseText = interbankBoundary.query(InterbankConfigs.PROCESS_TRANSACTION_URL, requestPayload);
